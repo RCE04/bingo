6980656748695a066ab292664770b5e1833554af
RCO04 <rodrigogameelcrack@gmail.com>
2023-12-16 13:38:19 +0100

    Añadir y perfeccioanr codigo js (marcar)

--- e11.js
+++ e11.js
@@ -3,11 +3,13 @@ var nuevoElemento
 var numero
 
 const ruleta = function () {
+    let cont = 0;
 
     setInterval(() => {
-        numero = (Math.floor(Math.random() * 89) + 1)
+        numero = (Math.floor(Math.random() * 99) + 1)
         let boton = document.getElementById("boton")
-        if (sali.length !== 100) {
+
+        if (sali.length !== 99) {
             comprobar()
             sali.push(numero);
             boton.textContent = numero
@@ -15,11 +17,19 @@ const ruleta = function () {
             let nuevaEtiqueta = document.createElement("div")
             nuevaEtiqueta.textContent = numero
             document.getElementById("des").appendChild(nuevaEtiqueta)
+
+            marcar()
+
+            if (cont == 45) {
+                let saltoL = document.createElement("br")
+                document.getElementById("des").appendChild(saltoL)
+                cont = 0
+            }
+            cont++
         }
 
 
-
-    }, 1);
+    }, 1)
 }
 
 const asignar = function () {
@@ -28,13 +38,14 @@ const asignar = function () {
         let sali2 = []
         for (let index = 0; index < 15; index++) {
             let nuevaEtiqueta = document.createElement("div")
-            let generarN = (Math.floor(Math.random() * 89) + 1)
+            let generarN = (Math.floor(Math.random() * 99) + 1)
 
             if (sali2.includes(generarN)) {
                 index--;
             } else {
                 nuevaEtiqueta.textContent = generarN;
                 document.getElementById("cartaJ").appendChild(nuevaEtiqueta)
+                nuevaEtiqueta.setAttribute("class", generarN)
                 sali2.push(generarN)
 
             }
@@ -47,13 +58,14 @@ const asignar = function () {
         let sali2 = []
         for (let index = 0; index < 15; index++) {
             let nuevaEtiqueta = document.createElement("div")
-            let generarN = (Math.floor(Math.random() * 89) + 1)
+            let generarN = (Math.floor(Math.random() * 99) + 1)
 
             if (sali2.includes(generarN)) {
                 index--;
             } else {
                 nuevaEtiqueta.textContent = generarN;
                 document.getElementById("cartaCPU").appendChild(nuevaEtiqueta)
+                nuevaEtiqueta.setAttribute("class", generarN)
                 sali2.push(generarN)
 
             }
@@ -64,15 +76,26 @@ const asignar = function () {
 }
 
 function comprobar() {
-    if (sali.length !== 100) {
+    if (sali.length !== 99) {
         for (let i = 0; i < sali.length; i++)
             if (sali[i] == numero) {
-                numero = (Math.floor(Math.random() * 89) + 1)
+                numero = (Math.floor(Math.random() * 99) + 1)
                 comprobar()
             }
     }
+}
+
+function marcar() {
+    let div = document.getElementsByClassName(numero)
+
+    if (div.length > 0) {
+        for (i = 0; i < div.length; i++) {
+            div[i].style.backgroundColor = 'rgb(255, 0, 0)'
+        }
+    }
 
 }
+
 
 
 
